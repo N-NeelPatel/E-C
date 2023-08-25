d20df07fba338658a1f461a15451febf2d9069ff
Neel Patel <neel199907@gmail.com>
2023-08-26 00:05:24 +0530

    added support for image path

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static # imported function that allows us to connect to our url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('base.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
