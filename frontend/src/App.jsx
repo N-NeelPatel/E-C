45ce986b4f11f8462320f656f4780a087f116bda
Neel Patel <neel199907@gmail.com>
2023-09-12 12:07:02 +0530

    added Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { Container } from "react-bootstrap"
 
 function App() {
 
   return (
     <>
       <Header />
+        <main>
+          <Container className="py-3">
+            <h1>Welcome</h1>
+          </Container>
+        </main>
       <Footer />
     </>
   )
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown, Row } from "react-bootstrap";
 
 export default function Header() {
     return (
-        <div>
-            <header>Header</header>
-        </div>
+        <header>
+            <Navbar expand="lg" className="bg-body-tertiary" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">eShop</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/cart"><i className='fas fa-shopping-cart' />Cart</Nav.Link>
+                        <Nav.Link href="/login"><i className='fas fa-user' />Login</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     )
 }
