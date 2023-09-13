3705eea4d6f4d6b8fadc26e27d197e03823541f3
Neel Patel <neel199907@gmail.com>
2023-09-13 18:10:52 +0530

    added api calls for fetching the products info

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import products from "../products";
 import Product from "../components/Product";
+import axios from 'axios';
 
 
 export default function HomeScreen() {
+    // const [products, setProducts] = useState([]);
+
+    // useEffect(()=> {
+
+    //     const fetchProducts = async() => {
+    //         try {
+    //             const { data } = await axios.get('http://127.0.0.1:8000/api/products/')
+    //             console.log('data', data);
+    //             setProducts(data);
+    //         } catch(e) {
+    //             console.error(e)
+    //         }
+    //     }
+    //     fetchProducts();
+    // }, []);
+    
     return (
         <div>
             <h1>Latest Products</h1>
