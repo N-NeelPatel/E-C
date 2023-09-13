3d6a0d1cf715c93fc1f143b92124670658dc4d68
Neel Patel <neel199907@gmail.com>
2023-09-13 16:01:19 +0530

    started adding routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,22 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen.'
 import { Container } from "react-bootstrap"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
 
   return (
-    <>
+    <Router>
       <Header />
         <main>
           <Container className="py-3">
-            <HomeScreen />
+            <Routes>
+              <Route path='/' element={<HomeScreen />} exact />
+            </Routes>
           </Container>
         </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
