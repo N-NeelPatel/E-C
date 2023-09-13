8ee8b3a7b73a979f34da085af832d758e1cf0f72
Neel Patel <neel199907@gmail.com>
2023-09-13 16:28:17 +0530

    added LinkContainer in Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function Header() {
     return (
         <header>
             <Navbar expand="lg" className="bg-body-tertiary" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">eShop</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>eShop</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/cart"><i className='fas fa-shopping-cart' />Cart</Nav.Link>
-                        <Nav.Link href="/login"><i className='fas fa-user' />Login</Nav.Link>
+                        <LinkContainer to="/cart">
+                            <Nav.Link><i className='fas fa-shopping-cart' />Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                            <Nav.Link><i className='fas fa-user' />Login</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Card } from "react-bootstrap";
 import Rating from "./Rating";
+import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function Product ({product}) {
